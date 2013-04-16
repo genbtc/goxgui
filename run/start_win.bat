3d82b84d785f6b94cff35b0ca50c003908163fdd
gen BTC <genBTC@hushmail.com>
2013-04-15 23:03:36 -0400

    Customized MANY things & UI improvements.
    1) Added a bitcoin icon. 2) Changed windows startup script. 3) Changed
    Order Book Layout to: Not Show any USD volumes, just show
    Price(USD),Size (BTC )& Total Sum(BTC). Ignore all small orders (Below
    0.6BTC and cumulate them into the next line with a volume weighted
    average price). Make the Price column 5 Decimals 4) Make Buy/Sell Price
    EditBox 5 decimals  5) Make  Minimum Window Size able to be resized MUCH
    smaller:  UI Changes: Font size smaller on some things

--- run/start_win.bat
+++ run/start_win.bat
@@ -1,3 +1,7 @@
 set PYTHONPATH=%PYTHONPATH%;..\goxgui;..\goxtool
 cd ..\goxgui\
+<<<<<<< HEAD
 start application.pyw
+=======
+start pythonw application.py
+>>>>>>> Customized MANY things & UI improvements.
